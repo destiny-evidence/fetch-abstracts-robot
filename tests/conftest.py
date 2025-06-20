9b9ee1680fa85682a2201effc5add1cfdeef1e0a
Harry Moss <h.moss@ucl.ac.uk>
2025-06-20 15:28:20 +0100

    Pass dummy destiny repo url in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,3 +6,4 @@ import pytest
 @pytest.hookimpl(tryfirst=True)
 def pytest_configure():
     os.environ["ENV"] = "local"
+    os.environ["DESTINY_REPOSITORY_URL"] = "http://localhost:8001/enhancement/"
