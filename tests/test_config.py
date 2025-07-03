ecc2bcefd89ab8d01111d361f463b1a347ce1427
nikloynes <niklasloynes@gmail.com>
2025-07-03 12:55:54 +0100

    tests for config/settings

---
+++ tests/test_config.py
@@ -0,0 +1,31 @@
+"""tests for app/config.py"""
+
+from app.config import get_settings
+
+
+def test_get_settings(set_test_environment_variables: None) -> None:
+    """
+    Test the get_settings function.
+
+    Args:
+        set_test_environment_variables (None): Pytest fixture to test environment variables.
+
+    """
+    expected_env = "local"
+    expected_destiny_repository_url = "http://localhost:8001/enhancement/"
+    expected_robot_id = "e0aba318-eee9-4b4c-b503-7f72547063d8"
+    expected_robot_secret = "dummy_secret"  # noqa: S105
+    expected_elsevier_scopus_key = "dummy_scopus_key"
+
+    settings = get_settings()
+
+    assert settings.env == expected_env
+    assert (
+        settings.destiny_repository_url.encoded_string()
+        == expected_destiny_repository_url
+    )
+    assert str(settings.robot_id) == expected_robot_id
+    assert settings.robot_secret == expected_robot_secret
+    assert (
+        settings.elsevier_scopus_key.get_secret_value() == expected_elsevier_scopus_key
+    )
